a27fa6de7d5927d6cfca17a3486bf9e99739e797
Berfin Tek <tekberfin@outlook.com>
2023-03-24 17:04:39 +0300

    Sezar_Sifreleme project added

---
+++ Sezar_Sifreleme/Sezar_Sifreleme/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Sezar_Sifreleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtVeri = new System.Windows.Forms.TextBox();
+            this.BtnSifrele = new System.Windows.Forms.Button();
+            this.TxtSifreliVeri = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnSifreCoz = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Veri:";
+            // 
+            // TxtVeri
+            // 
+            this.TxtVeri.Location = new System.Drawing.Point(207, 56);
+            this.TxtVeri.Name = "TxtVeri";
+            this.TxtVeri.Size = new System.Drawing.Size(280, 30);
+            this.TxtVeri.TabIndex = 1;
+            // 
+            // BtnSifrele
+            // 
+            this.BtnSifrele.Location = new System.Drawing.Point(207, 150);
+            this.BtnSifrele.Name = "BtnSifrele";
+            this.BtnSifrele.Size = new System.Drawing.Size(133, 38);
+            this.BtnSifrele.TabIndex = 2;
+            this.BtnSifrele.Text = "Şifrele";
+            this.BtnSifrele.UseVisualStyleBackColor = true;
+            this.BtnSifrele.Click += new System.EventHandler(this.BtnSifrele_Click);
+            // 
+            // TxtSifreliVeri
+            // 
+            this.TxtSifreliVeri.Location = new System.Drawing.Point(207, 104);
+            this.TxtSifreliVeri.Name = "TxtSifreliVeri";
+            this.TxtSifreliVeri.Size = new System.Drawing.Size(280, 30);
+            this.TxtSifreliVeri.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Şireli Veri:";
+            // 
+            // BtnSifreCoz
+            // 
+            this.BtnSifreCoz.Location = new System.Drawing.Point(354, 150);
+            this.BtnSifreCoz.Name = "BtnSifreCoz";
+            this.BtnSifreCoz.Size = new System.Drawing.Size(133, 38);
+            this.BtnSifreCoz.TabIndex = 5;
+            this.BtnSifreCoz.Text = "Şifre Çöz";
+            this.BtnSifreCoz.UseVisualStyleBackColor = true;
+            this.BtnSifreCoz.Click += new System.EventHandler(this.BtnSifreCoz_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 238);
+            this.Controls.Add(this.BtnSifreCoz);
+            this.Controls.Add(this.TxtSifreliVeri);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnSifrele);
+            this.Controls.Add(this.TxtVeri);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtVeri;
+        private System.Windows.Forms.Button BtnSifrele;
+        private System.Windows.Forms.TextBox TxtSifreliVeri;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnSifreCoz;
+    }
+}
+
---
+++ Sezar_Sifreleme/Sezar_Sifreleme/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sezar_Sifreleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSifrele_Click(object sender, EventArgs e)
+        {
+            string veri = "";
+            veri=TxtVeri.Text;
+            char[] karakterler=veri.ToCharArray();
+            foreach(char eleman in karakterler)
+            {
+                TxtSifreliVeri.Text += Convert.ToChar(eleman + 3).ToString();
+            }
+        }
+
+        private void BtnSifreCoz_Click(object sender, EventArgs e)
+        {
+            string sifre = "";
+            sifre = TxtSifreliVeri.Text;
+            char[] karakterler=sifre.ToCharArray();
+            foreach(char eleman in karakterler)
+            {
+                TxtVeri.Text += Convert.ToChar(eleman - 3).ToString();
+
+            }
+        }
+    }
+}
